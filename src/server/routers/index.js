b0b8140356c1e0621cd1f8bea4c0406fb8950789
薛飞 <xuefei@corp.netease.com>
2018-09-18 12:51:45 +0800

    :sparkles: add jsonp

--- src/server/routers/index.js
+++ src/server/routers/index.js
@@ -5,6 +5,27 @@ const graphqlController = require('@controller/graphql');
 const home = require('./home');
 const api = require('./api');
 
+router.get('/house.js', (ctx, next) => {
+    const data = [
+        { address: '12-3-4', area: 50 },
+        { address: '12-3-4', area: 70 },
+        { address: '12-3-5', area: 90 },
+        { address: '12-3-4', area: 5 },
+    ];
+    ctx.body = `fn(${JSON.stringify(data)})`;
+});
+
+router.get('/house1.js', (ctx, next) => {
+    const callabckName = ctx.query.callback;
+    const data = [
+        { address: '12-3-4', area: 50 },
+        { address: '12-3-4', area: 70 },
+        { address: '12-3-5', area: 90 },
+        { address: '12-3-4', area: 5 },
+    ];
+    ctx.body = `${callabckName}(${JSON.stringify(data)})`;
+});
+
 router.use('/v1/api', api.routes());
 router.use('', home.routes());
 // graphql
